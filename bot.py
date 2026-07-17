18fbc593eafff859979566722a417d1eb3055fdb
alejo2026UwU <alejoferreiro062@gmail.com>
2026-07-16 21:36:06 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -235,7 +235,7 @@ async def conectar_node():
     """Conecta a Lavalink con reintentos y compatibilidad v4 forzada 🛡️🎵"""
     nodos_config = [
         {
-            "uri": "http://lava.link:80", 
+            "uri": "ssl://lavalink.yandere.cc:443", 
             "password": "youshallnotpass"
         }
     ]
